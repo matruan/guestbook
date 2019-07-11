08b836883e810abca91c27f00f6a6783588cde82
matruan <ruizmat@gmail.com>
2019-07-11 12:33:28 +0200

    first commit

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author mruiz
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "Guestbook";
+
+}
